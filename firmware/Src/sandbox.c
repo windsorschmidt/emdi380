635520fb06a531670bf02db47f925cab592cba06
Windsor Schmidt <windsor.schmidt@gmail.com>
2017-07-02 01:29:43 -0700

    Add sandbox source file for testing

---
+++ firmware/Src/sandbox.c
@@ -0,0 +1,19 @@
+#include "stm32f4xx_hal.h"
+#include "cmsis_os.h"
+
+// Support OpenOCD's thread awareness
+const int __attribute__((used)) uxTopUsedPriority = configMAX_PRIORITIES - 1;
+
+// FreeRTOS idle task, defined __weak in auto-generated main.c
+void DefaultTask(void const * argument) {
+    while(1) {
+        GPIOE->BSRR = (1<<0); // green LED ON
+        osDelay(200);
+        GPIOE->BSRR = (1<<16); // green LED off
+        osDelay(200);
+        GPIOE->BSRR = (1<<1); // red LED on
+        osDelay(200);
+        GPIOE->BSRR = (1<<(1+16)); // red LED off
+        osDelay(200);
+    }
+}
